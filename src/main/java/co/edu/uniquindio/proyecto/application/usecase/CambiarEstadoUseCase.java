f7447fccde01fdd00d1c1829aadf257c1f0ed825
ValentinaOrlas <johanavop25@gmail.com>
2026-03-04 22:32:23 -0500

    Agregué y codifiqué repository y usecase, terminados y ensayados

--- src/main/java/co/edu/uniquindio/proyecto/application/usecase/CambiarEstadoUseCase.java
+++ src/main/java/co/edu/uniquindio/proyecto/application/usecase/CambiarEstadoUseCase.java
@@ -1,4 +1,28 @@
 package co.edu.uniquindio.proyecto.application.usecase;
 
+import co.edu.uniquindio.proyecto.domain.entity.Solicitud;
+import co.edu.uniquindio.proyecto.domain.entity.Usuario;
+import co.edu.uniquindio.proyecto.domain.repository.solicitud.SolicitudRepository;
+import co.edu.uniquindio.proyecto.domain.valueobject.*;
+
 public class CambiarEstadoUseCase {
+
+    private final SolicitudRepository repository;
+
+    public CambiarEstadoUseCase(SolicitudRepository repository) {
+        this.repository = repository;
+    }
+
+    public void ejecutar(SolicitudId id,
+                         EstadoSolicitud nuevoEstado,
+                         String observacion,
+                         Usuario usuario) {
+
+        Solicitud solicitud = repository.obtenerPorId(id)
+                .orElseThrow(() -> new RuntimeException("Solicitud no encontrada"));
+
+        solicitud.cambiarEstado(nuevoEstado, observacion, usuario);
+
+        repository.guardar(solicitud);
+    }
 }
