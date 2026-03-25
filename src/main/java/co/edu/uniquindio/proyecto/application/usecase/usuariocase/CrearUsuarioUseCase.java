ea5ce04c3f8d2917a2f9a2398f7874981beb3cf5
ValentinaOrlas <johanavop25@gmail.com>
2026-03-25 11:54:12 -0500

    Registro usuario completo

---
+++ src/main/java/co/edu/uniquindio/proyecto/application/usecase/usuariocase/CrearUsuarioUseCase.java
@@ -0,0 +1,39 @@
+package co.edu.uniquindio.proyecto.application.usecase.usuariocase;
+
+import co.edu.uniquindio.proyecto.domain.entity.Usuario;
+import co.edu.uniquindio.proyecto.domain.exception.ReglaDominioException;
+import co.edu.uniquindio.proyecto.domain.repository.usuario.UsuarioRepository;
+import co.edu.uniquindio.proyecto.domain.valueobject.DocumentoIdentidad;
+import co.edu.uniquindio.proyecto.domain.valueobject.Email;
+import co.edu.uniquindio.proyecto.domain.valueobject.TipoUsuario;
+import lombok.RequiredArgsConstructor;
+
+
+public class CrearUsuarioUseCase {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public CrearUsuarioUseCase(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public void ejecutar(DocumentoIdentidad id, String nombre, Email email, TipoUsuario tipo) {
+        // La validación de duplicados aquí es una regla de "flujo"
+        if (usuarioRepository.existePorIdentificacion(id)) {
+            throw new ReglaDominioException("Usuario ya existe");
+        }
+
+        Usuario nuevo = Usuario.crear(id, nombre, email, tipo);
+        usuarioRepository.guardar(nuevo);
+    }
+
+    private void validarExistencia(DocumentoIdentidad id, Email email) {
+        if (usuarioRepository.existePorIdentificacion(id)) {
+            throw new ReglaDominioException("Ya existe un usuario con esa identificación: ");
+        }
+
+        if (usuarioRepository.existePorEmail(email)) {
+            throw new ReglaDominioException("El email " + email.getValor() + " ya está registrado");
+        }
+    }
+}
--- src/main/java/co/edu/uniquindio/proyecto/infraestructure/repository/UsuarioRepositoryEnMemoria.java
+++ src/main/java/co/edu/uniquindio/proyecto/infraestructure/repository/UsuarioRepositoryEnMemoria.java
@@ -3,9 +3,12 @@ package co.edu.uniquindio.proyecto.infraestructure.repository;
 import co.edu.uniquindio.proyecto.domain.entity.Usuario;
 import co.edu.uniquindio.proyecto.domain.repository.usuario.UsuarioRepository;
 import co.edu.uniquindio.proyecto.domain.valueobject.DocumentoIdentidad;
+import co.edu.uniquindio.proyecto.domain.valueobject.Email; // Importante importar el VO
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class UsuarioRepositoryEnMemoria implements UsuarioRepository {
 
     private final Map<DocumentoIdentidad, Usuario> baseDatos = new HashMap<>();
@@ -21,6 +24,23 @@ public class UsuarioRepositoryEnMemoria implements UsuarioRepository {
     }
 
     @Override
+    public Optional<Usuario> obtenerPorEmail(Email email) {
+        return baseDatos.values().stream()
+                .filter(u -> u.getEmail().equals(email))
+                .findFirst();
+    }
+
+    @Override
+    public boolean existePorIdentificacion(DocumentoIdentidad id) {
+        return baseDatos.containsKey(id);
+    }
+
+    @Override
+    public boolean existePorEmail(Email email) {
+        return baseDatos.values().stream()
+                .anyMatch(u -> u.getEmail().equals(email));
+    }
+
     public List<Usuario> obtenerTodos() {
         return new ArrayList<>(baseDatos.values());
     }
