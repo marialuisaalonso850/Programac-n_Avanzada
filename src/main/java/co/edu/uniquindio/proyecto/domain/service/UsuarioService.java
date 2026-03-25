aaad80bcfe0e198415c764e671b135fbee1efc61
ValentinaOrlas <johanavop25@gmail.com>
2026-03-25 11:53:41 -0500

    Servicio de crear agregado

--- src/main/java/co/edu/uniquindio/proyecto/domain/service/UsuarioService.java
+++ src/main/java/co/edu/uniquindio/proyecto/domain/service/UsuarioService.java
@@ -1,8 +1,18 @@
 package co.edu.uniquindio.proyecto.domain.service;
 import co.edu.uniquindio.proyecto.domain.entity.Usuario;
 import co.edu.uniquindio.proyecto.domain.exception.ReglaDominioException;
+import co.edu.uniquindio.proyecto.domain.repository.usuario.UsuarioRepository;
+import co.edu.uniquindio.proyecto.domain.valueobject.DocumentoIdentidad;
+import co.edu.uniquindio.proyecto.domain.valueobject.Email;
+import co.edu.uniquindio.proyecto.domain.valueobject.TipoUsuario;
 
 public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
 
     public void desactivarUsuario(Usuario usuario, boolean tieneSolicitudesActivas) {
 
@@ -13,5 +23,23 @@ public class UsuarioService {
         }
 
         usuario.desactivar();
+    }
+
+    public void ejecutar(DocumentoIdentidad id, String nombre, Email email, TipoUsuario tipo) {
+
+        // 1. Validar si ya existe por ID
+        if (usuarioRepository.existePorIdentificacion(id)) {
+            throw new ReglaDominioException("Ya existe un usuario con esa identificación");
+        }
+
+        // 2. Validar si el email ya está en uso
+        if (usuarioRepository.existePorEmail(email)) {
+            throw new ReglaDominioException("El email ya se encuentra registrado");
+        }
+
+        Usuario nuevoUsuario = Usuario.crear(id, nombre, email, tipo);
+
+        // 4. Persistir
+        usuarioRepository.guardar(nuevoUsuario);
     }
 }
