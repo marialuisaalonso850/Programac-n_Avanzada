cd5d102bb1c14ed1ffd1db09228a665bae73e935
ValentinaOrlas <johanavop25@gmail.com>
2026-04-07 17:14:44 -0500

    Se agregó el servicio para validar si el usuario existe con un id correspondiente

--- src/main/java/co/edu/uniquindio/proyecto/domain/service/usuario/existebyid/ValidarUsuarioByIdService.java
+++ src/main/java/co/edu/uniquindio/proyecto/domain/service/usuario/existebyid/ValidarUsuarioByIdService.java
@@ -4,13 +4,16 @@ import co.edu.uniquindio.proyecto.domain.exception.ReglaDominioException;
 import co.edu.uniquindio.proyecto.domain.repository.usuario.UsuarioRepository;
 import co.edu.uniquindio.proyecto.domain.valueobject.DocumentoIdentidad;
 import lombok.RequiredArgsConstructor;
+import java.util.Objects;
 
 @RequiredArgsConstructor
-public class ExisteByIdService {
+public class ValidarUsuarioByIdService {
 
     private final UsuarioRepository usuarioRepository;
 
-    public void existeById(DocumentoIdentidad id){
+    public void ejecutar(DocumentoIdentidad id){
+
+        Objects.requireNonNull(id, "El documento de identidad no puede ser nulo");
 
         if (usuarioRepository.existePorIdentificacion(id)) {
             throw new ReglaDominioException("Ya existe un usuario con esa identificación");
